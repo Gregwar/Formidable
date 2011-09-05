8616f9d6a923d6bc0f8fab4ed8168f7532fabd3a
Grégoire Passault <g.passault@gmail.com>
2011-09-05 09:41:10 +0200

    [Refactorisation] CAPTCHA

--- Captcha/Captcha.php
+++ Captcha/Captcha.php
@@ -2,109 +2,127 @@
 
 namespace Gregwar\DSD\Captcha;
 
-function getCol($image, $x, $y) {
-	$L = imagesx($image);
-	$H = imagesy($image);
-	if ($x<0 || $x>=$L || $y<0 || $y>=$H)
-		return 0xFFFFFF;
-	else return imagecolorat($image, $x, $y);
-}
+/**
+ * Génération d'un Captcha
+ *
+ * @author Grégoire Passault <g.passault@gmail.com>
+ */
+class Captcha
+{
+    private $value;
 
-class Captcha {
-	public $value;
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
 
-	public static $charset = "0123456789abcdefghijkmnpqrstuvwxyz";
+    /**
+     * Retourne le code de l'image en HTML qu'on peut embeder
+     */
+    public function inline()
+    {
+        return 'data:image/png;base64,'.base64_encode($this->raw());
+    }
 
-	public static function generate() {
-		$n = strlen(DSDCaptcha::$charset);
-		$s = "";
-		for ($i=0; $i<5; $i++) {
-			$s .= DSDCaptcha::$charset[mt_rand(0,$n-1)];
-		}
-		return $s;
-	}
+    /**
+     * Retourne le contenu de l'image brut
+     */
+    public function raw()
+    {
+        $i = imagecreatetruecolor(120,40);
+        $back = imagecolorallocate($i, 255, 255, 255);
+        $col = imagecolorallocate($i, mt_rand(0,110), mt_rand(0,110), mt_rand(0,110));
+        imagefill($i, 0, 0, $back);
 
-	public function __construct() {
-		$this->value = DSDCaptcha::generate();
-	}
+        // Lignes aléatoire
+        for ($t=0; $t<10; $t++) {
+            $tcol = imagecolorallocate($i, 100+mt_rand(0,150), 100+mt_rand(0,150), 100+mt_rand(0,150));
+            $Xa = mt_rand(0, 120);
+            $Ya = mt_rand(0, 40);
+            $Xb = mt_rand(0, 120);
+            $Yb = mt_rand(0, 40);
+            imageline($i, $Xa, $Ya, $Xb, $Yb, $tcol);
+        }
 
-	public function display() {
-		$i = imagecreatetruecolor(120,40);
-		$back = imagecolorallocate($i, 255, 255, 255);
-		$col = imagecolorallocate($i, mt_rand(0,110), mt_rand(0,110), mt_rand(0,110));
-		imagefill($i, 0, 0, $back);
-		for ($t=0; $t<10; $t++) {
-			$tcol = imagecolorallocate($i, 100+mt_rand(0,150), 100+mt_rand(0,150), 100+mt_rand(0,150));
-			$Xa = mt_rand(0, 120);
-			$Ya = mt_rand(0, 40);
-			$Xb = mt_rand(0, 120);
-			$Yb = mt_rand(0, 40);
-			imageline($i, $Xa, $Ya, $Xb, $Yb, $tcol);
-		}
-		imagettftext($i, 28, 0, 5, 32, $col, dirname(__FILE__)."/captcha.ttf", $this->value);
+        // Code
+        imagettftext($i, 28, 0, 5, 32, $col, __DIR__.'/Fonts/captcha.ttf', $this->value);
 
-		$X = mt_rand(0, 120);
-		$Y = mt_rand(0, 40);
-		$Phase=mt_rand(0,10);
-		$Scale = 1.3 + mt_rand(0,10000)/30000;
-		$Amp=1+mt_rand(0,1000)/1000;
-		$out = imagecreatetruecolor(120,40);
-		for ($x=0; $x<120; $x++)
-		for ($y=0; $y<40; $y++) {
-			$Vx=$x-$X;
-			$Vy=$y-$Y;
-			$Vn=sqrt($Vx*$Vx+$Vy*$Vy);
-			if ($Vn!=0) {
-				$Vn2=$Vn+4*sin($Vn/8);
-				$nX=$X+($Vx*$Vn2/$Vn);
-				$nY=$Y+($Vy*$Vn2/$Vn);
-			} else {
-				$nX=$X;
-				$nY=$Y;
-			}
-			$nY = $nY+$Scale*sin($Phase + $nX*0.2);
-			$p = $this->bilinearInterpolate($nX-floor($nX), $nY-floor($nY), getCol($i,floor($nX),floor($nY)),
-								  getCol($i,ceil($nX),floor($nY)),
-								  getCol($i,floor($nX),ceil($nY)),
-								  getCol($i,ceil($nX),ceil($nY)));
+        // Brouillage
+        $X = mt_rand(0, 120);
+        $Y = mt_rand(0, 40);
+        $Phase=mt_rand(0,10);
+        $Scale = 1.3 + mt_rand(0,10000)/30000;
+        $Amp=1+mt_rand(0,1000)/1000;
 
-			if ($p==0) $p=0xFFFFFF;
-			imagesetpixel($out, $x, $y, $p);
-		}
+        $out = imagecreatetruecolor(120,40);
+        for ($x=0; $x<120; $x++) {
+            for ($y=0; $y<40; $y++) {
+                $Vx=$x-$X;
+                $Vy=$y-$Y;
+                $Vn=sqrt($Vx*$Vx+$Vy*$Vy);
+                if ($Vn!=0) {
+                    $Vn2=$Vn+4*sin($Vn/8);
+                    $nX=$X+($Vx*$Vn2/$Vn);
+                    $nY=$Y+($Vy*$Vn2/$Vn);
+                } else {
+                    $nX=$X;
+                    $nY=$Y;
+                }
+                $nY = $nY+$Scale*sin($Phase + $nX*0.2);
+                $p = $this->bilinearInterpolate($nX-floor($nX), $nY-floor($nY), 
+                $this->getCol($i,floor($nX),floor($nY)),
+                $this->getCol($i,ceil($nX),floor($nY)),
+                $this->getCol($i,floor($nX),ceil($nY)),
+                $this->getCol($i,ceil($nX),ceil($nY)));
 
-		header("Content-type: image/jpeg");
-		imagejpeg($out, null, 25);
-	}
+                if ($p==0) $p=0xFFFFFF;
+                imagesetpixel($out, $x, $y, $p);
+            }
+        }
 
-	function bilinearInterpolate($x, $y, $nw, $ne, $sw, $se) {
-	  $r0 = (int)($nw >> 16) & 0xff;
-	  $g0 = (int)($nw >>  8) & 0xff;
-	  $b0 = (int)($nw      ) & 0xff;
-	  $r1 = (int)($ne >> 16) & 0xff;
-	  $g1 = (int)($ne >>  8) & 0xff;
-	  $b1 = (int)($ne      ) & 0xff;
-	  $r2 = (int)($sw >> 16) & 0xff;
-	  $g2 = (int)($sw >>  8) & 0xff;
-	  $b2 = (int)($sw      ) & 0xff;
-	  $r3 = (int)($se >> 16) & 0xff;
-  	  $g3 = (int)($se >>  8) & 0xff;
-	  $b3 = (int)($se      ) & 0xff;
-	 
-	  $cx = 1.0 - $x;
-	  $cy = 1.0 - $y;
-	 
-	  $m0 = $cx * $r0 + $x * $r1;
-	  $m1 = $cx * $r2 + $x * $r3;
-	  $r = (int)($cy * $m0 + $y * $m1);
- 	
-	  $m0 = $cx * $g0 + $x * $g1;
-	  $m1 = $cx * $g2 + $x * $g3;
-	  $g = (int)($cy * $m0 + $y * $m1);
-	 
-	  $m0 = $cx * $b0 + $x * $b1;
-	  $m1 = $cx * $b2 + $x * $b3;
-	  $b = (int)($cy * $m0 + $y * $m1);
-	 
-	  return ($r << 16) | ($g << 8) | $b;
-	}
+        ob_start();
+        imagejpeg($out, null, 10);
+        return ob_get_clean();
+    }
+
+    private function bilinearInterpolate($x, $y, $nw, $ne, $sw, $se) {
+        $r0 = (int)($nw >> 16) & 0xff;
+        $g0 = (int)($nw >>  8) & 0xff;
+        $b0 = (int)($nw      ) & 0xff;
+        $r1 = (int)($ne >> 16) & 0xff;
+        $g1 = (int)($ne >>  8) & 0xff;
+        $b1 = (int)($ne      ) & 0xff;
+        $r2 = (int)($sw >> 16) & 0xff;
+        $g2 = (int)($sw >>  8) & 0xff;
+        $b2 = (int)($sw      ) & 0xff;
+        $r3 = (int)($se >> 16) & 0xff;
+        $g3 = (int)($se >>  8) & 0xff;
+        $b3 = (int)($se      ) & 0xff;
+
+        $cx = 1.0 - $x;
+        $cy = 1.0 - $y;
+
+        $m0 = $cx * $r0 + $x * $r1;
+        $m1 = $cx * $r2 + $x * $r3;
+        $r = (int)($cy * $m0 + $y * $m1);
+
+        $m0 = $cx * $g0 + $x * $g1;
+        $m1 = $cx * $g2 + $x * $g3;
+        $g = (int)($cy * $m0 + $y * $m1);
+
+        $m0 = $cx * $b0 + $x * $b1;
+        $m1 = $cx * $b2 + $x * $b3;
+        $b = (int)($cy * $m0 + $y * $m1);
+
+        return ($r << 16) | ($g << 8) | $b;
+    }
+
+    private function getCol($image, $x, $y)
+    {
+        $L = imagesx($image);
+        $H = imagesy($image);
+        if ($x<0 || $x>=$L || $y<0 || $y>=$H)
+            return 0xFFFFFF;
+        else return imagecolorat($image, $x, $y);
+    }
 }
--- Fields/CaptchaField.php
+++ Fields/CaptchaField.php
@@ -2,32 +2,61 @@
 
 namespace Gregwar\DSD\Fields;
 
+include(__DIR__.'/../Captcha/Captcha.php');
+
+use Gregwar\DSD\Captcha\Captcha;
+
+/**
+ * Champ de type CAPTCHA
+ *
+ * @author Grégoire Passault <g.passault@gmail.com>
+ */
 class CaptchaField extends Field
 {
+    private $captchaValue = '';
 
     public function __construct()
     {
-		$this->type = 'text';
-	}
-	
+        $this->type = 'text';
+        $this->generate();
+    }
+
+    /**
+     * Génère la valeur du CAPTCHA
+     */
+    private function generate()
+    {
+        $chars = str_split('0123456789abcdefghijkmnpqrstuvwxyz');
+
+        for ($i=0; $i<5; $i++) {
+            $this->captchaValue .= $chars[array_rand($chars)];
+        }
+
+        $_SESSION['DSD_Captcha'] = $this->captchaValue;
+    }
+
     public function check()
     {
-		$this->value=strtolower($this->value);
-		$this->value=strtr($this->value,"lo","10");
-		if (!isset($_SESSION['DSD_Captcha']) || $_SESSION['DSD_Captcha']!=$this->value) {
-			return 'La valeur du code visuel n\'est pas la bonne';
-		}
-		unset($_SESSION["DSD_Captcha"]);
-	}
+        $this->value = strtolower($this->value);
+        $this->value = strtr($this->value, 'lo', '10');
+
+        if (!isset($_SESSION['DSD_Captcha']) || $_SESSION['DSD_Captcha']!=$this->value) {
+            return 'La valeur du code visuel n\'est pas la bonne';
+        }
+        unset($_SESSION["DSD_Captcha"]);
+    }
 
     public function getHTML()
     {
-		global $DSDHttp;
-		$nodraw = $this->value;
-		$this->value="";
-		$val = parent::getHTML();
-        $this->value = $nodraw;
-        // XXX: Implémentation du CAPTCHA inline
-//		return "<img src=\"".$DSDHttp."/captcha.php?".time()."\" class=\"DSD_Captcha\" alt=\"Code visuel\" ><br />$val";
-	}
+        $temp = $this->value;
+        $this->value = '';
+        $input_html = parent::getHTML();
+        $this->value = $temp;
+
+        $captcha = new Captcha($this->captchaValue);
+
+        $html = '<img src="'.$captcha->inline().'" class="DSD_Captcha" alt="Code visuel" ><br />'.$input_html;
+
+        return $html;
+    }
 }
