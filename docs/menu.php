e11835923e06902d35522582099712eef9df24e9
Gregwar <g.passault@gmail.com>
2013-04-17 16:04:58 +0200

    Fixing menu

--- docs/menu.php
+++ docs/menu.php
@@ -1,7 +1,7 @@
 <ol class="menu">
 <?php foreach ($pages as $page => $description) { ?>
 <li class="<?php if ($current == $page) { ?>current<?php } ?>">
-	<a href="<?php echo $page; ?>.html"><?php echo $description; ?></a>
+	<a href="?p=<?php echo $page; ?>"><?php echo $description; ?></a>
     </li>
 <?php } ?>
 </ol>
