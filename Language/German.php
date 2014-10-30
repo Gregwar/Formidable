6168978cdc13f65eb7e4d2b628e4a4c37df11af1
Dominik Stiller <domi.stiller@gmail.com>
2014-10-30 02:49:19 +0100

    Added german translations

---
+++ Language/German.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Gregwar\Formidable\Language;
+
+class German extends Language
+{
+    protected $messages = array(
+        'read_only' => 'Das Feld %s ist nur lesbar und sollte nicht geändert werden',
+        'value_required' => 'Sie sollten einen Wert für das Feld %s eingeben',
+        'bad_format' => 'Das Format des Feldes %s ist nicht korrekt',
+        'at_least' => 'Das Feld %s sollte mindestens %d Zeichen lang sein',
+        'not_more' => 'Das Feld %s sollte nicht länger als %d Zeichen lang sein',
+        'bad_email' => 'Das Feld %s sollte eine gültige E-Mail-Adresse sein',
+        'bad_captcha' => 'Der Captcha-Wert ist nicht korrekt',
+        'bad_date' => 'Das Datum %s ist nicht korrekt',
+        'add' => 'Hinzufügen',
+        'remove' => 'Entfernen',
+        'file_size_too_big' => 'Die Dateigröße des Feldes %s sollte %s nicht überschreiten',
+        'file_image' => 'Die Datei des Feldes %s sollte ein Bild sein',
+        'file_required' => 'Sie sollten eine Datei für das Feld %s senden',
+        'integer' => 'Das Feld %s sollte eine Ganzzahl sein',
+        'should_check' => 'Sie sollten für %s ein Kästchen auswählen',
+        'number' => 'Das Feld %s sollte eine Nummer sein',
+        'number_min' => 'Das Feld %s sollte mindestens %s betragen',
+        'number_max' => 'Das Feld %s sollte nicht größer als %s sein',
+        'number_step' => 'Das Feld %s sollte ein Vielfaches von %f sein',
+        'should_choose' => 'Sie sollten ein Feld für %s auswählen',
+        'multiple_min' => 'Sie sollten mindestens %d Einträge für das Feld %s auswählen',
+        'multiple_mmax' => 'Sie sollten nicht mehr als %d Einträge für das Feld %s auswählen',
+        'bad_array_value', 'Der Wert des Feldes %s ist nicht korrekt',
+    );
+}
